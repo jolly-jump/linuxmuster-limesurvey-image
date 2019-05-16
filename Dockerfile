a578530876c4e35498618a7de06f42f9d54342b4
Tobias Küchel <t.kuechel@humboldt-ka.de>
2019-05-16 11:17:43 +0200

    new php, new limesurvey, rebuild

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM php:7.3.4-apache-stretch
+FROM php:7.3.5-apache-stretch
 RUN mkdir /var/linuxmuster-limesurvey
 RUN mkdir /usr/share/linuxmuster-limesurvey
 COPY --chown=www-data limesurvey/  /var/www/html/
--- getlimesurvey.sh
+++ getlimesurvey.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget https://www.limesurvey.org/stable-release?download=2546:limesurvey3171%20190408zip -O limesurvey.zip
+#wget https://www.limesurvey.org/stable-release?download=2546:limesurvey3171%20190408zip -O limesurvey.zip
+wget https://www.limesurvey.org/stable-release?download=2550:limesurvey3173%20190429zip -O limesurvey.zip
 
 unzip limesurvey.zip
