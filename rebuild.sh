0e6601dc7deed030894c373eb77da56776dbc4d3
Tobias Küchel <t.kuechel@humboldt-ka.de>
2019-10-07 01:42:46 +0200

    use changelog, add rebuild.sh script

---
+++ rebuild.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+./getlimesurvey.sh
+
+git status
+php_version=$(cat php_version.txt)
+grep $php_version Dockerfile || { echo "$php_version not in Dockerfile, please fix"; exit 1 ; }
+git commit -a -m"limesurvey new version: $new_version; php version: $php_version" 
+
+docker pull php:apache
+docker inspect php:apache | grep RepoTags -A 3
+git_log=$(git log --oneline | head -1 | cut -d " " -f 1)
+echo "Press enter to build with: docker build -t hgkvplan/limesurvey:$php_version-$git_log ."
+read
+docker build -t hgkvplan/linuxmuster-survey:$php_version-$git_log .
+docker tag hgkvplan/linuxmuster-survey:$php_version-$git_log hgkvplan/linuxmuster-survey:latest
+echo "Try if :latest works for you. Then press enter to tag and upload using"
+echo "docker tag hgkvplan/linuxmuster-survey:$php_version-$git_log hgkvplan/linuxmuster-survey:working"
+echo "docker push hgkvplan/linuxmuster-survey:$php_version-$git_log ; docker push hgkvplan/linuxmuster-survey:latest"
+read
+docker tag hgkvplan/linuxmuster-survey:$php_version-$git_log hgkvplan/linuxmuster-survey:working
+docker push hgkvplan/linuxmuster-survey:$php_version-$git_log ; docker push hgkvplan/linuxmuster-survey:latest
