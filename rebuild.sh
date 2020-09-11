f8dd42a6f1fb9013806fa8556ff8650cb58453f4
Tobias Küchel <t.kuechel@humboldt-ka.de>
2020-09-11 17:04:46 +0200

    limesurvey version: 200907; php version: 7.4.10-apache-buster

--- rebuild.sh
+++ rebuild.sh
@@ -15,7 +15,7 @@ docker inspect php:apache | grep RepoTags -A 3
 git_log=$(git log --oneline | head -1 | cut -d " " -f 1)
 echo "Press enter to build with: docker build -t linuxmuster/survey:$php_version-$git_log ."
 read
-docker build -t linuxmuster/survey:$php_version-$git_log .
+docker build --no-cache -t linuxmuster/survey:$php_version-$git_log .
 docker tag linuxmuster/survey:$php_version-$git_log linuxmuster/survey:latest
 echo "Try if :latest works for you. Then press enter to tag and upload using"
 echo "docker tag linuxmuster/survey:$php_version-$git_log linuxmuster/survey:working"
